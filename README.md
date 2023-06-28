ba2af7dd83c8834a1123eea6cb94f04d7d3779d1
Lucas <loukaka41@gmail.com>
2023-06-28 23:06:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# What I learned
+
+- Learning how client and server components work.
+- Learning basics of Tailwind
+
+'useclient' => client component
+if there's actions with user
+
+- Architecture of files
+- useCallback
+- Using of react-hook-form
+- Leaflet
+- query-string
+- Prisma
+- schemas with prisma
+- MongoDB
+
+Everything is from this video: https://youtu.be/c_-b_isI4vg
+By Code With Antonio
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import ClientOnly from './components/ClientOnly';
+import Navbar from './components/navbar/Navbar';
+import './globals.css'
+import { Nunito } from 'next/font/google';
+import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import RegisterModal from './components/modals/RegisterModal';
+import RentModal from './components/modals/RentModal';
+import getCurrentUser from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal';
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser()
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <SearchModal/>
+          <RentModal/>
+          <LoginModal/>
+          <RegisterModal/>
+        <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className='pb-20 pt-28'>
+        {children}
+        </div>
+        </body>
+    </html>
+  )
+}
